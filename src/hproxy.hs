feb66148e1c35f32ff8be0378920e7d4f0313515
ice <ice@fxhome.local>
2013-12-04 08:00:39 +0900

    login box

--- src/hproxy.hs
+++ src/hproxy.hs
@@ -1,7 +1,9 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE BangPatterns #-}
 
 module Main where
 
-import Control.Concurrent.HEP
+import Control.Concurrent.HEP as H
 --import ProxyClient
 import Network
 import HElementary.Elementary
@@ -11,54 +13,78 @@ import Data.ByteString.Char8 as C8
 import System.IO
 import Foreign.Ptr
 import Control.Monad
+import Control.Monad.Trans
+import Data.Typeable
+
+data GuiMessage = GuiExit
+                | GuiLogin ByteString
+    deriving Typeable
+instance Message GuiMessage
 
 login = "testuser"
 password = "Qwer1234"
 
-main = withSocketsDo $! withElementary $ do
-    elm_win_util_standard_add (fromString "mainwnd") (fromString "genProxy") >>= \parent-> do
-        -- on close
-        evas_object_smart_callback_add parent (fromString "delete,request") on_done nullPtr
-        -- set size
-        evas_object_size_hint_min_set parent 640 480
-        evas_object_resize parent 640 480
-        -- box
-        box <- elm_box_add parent
-        evas_object_size_hint_weight_set box c'EVAS_HINT_EXPAND c'EVAS_HINT_EXPAND
-        -- resize box to parent
-        elm_win_resize_object_add parent box
-        evas_object_show box
-        
-        forM [addMenu, addSearch, addStatus] $ \foo-> do
-            childbox <- foo parent
-            elm_box_pack_end box childbox
-        evas_object_show parent
-    
-    elm_run
-    return ()
+main = withSocketsDo $! withElementary $! runHEPGlobal $! 
+        procWithBracket guiInit procFinished $! H.proc $! do
+    msg <- receive
+    case fromMessage msg of
+        Nothing -> procRunning
+        Just GuiExit -> procFinished
+        Just (GuiLogin login) -> do
+            liftIO $! print login
+            procFinished
 
+guiInit = do
+    mybox <- selfMBox
+    worker <- spawn $! proc $! guiProc mybox
+    procRunning
+
+guiProc outbox = do
+    liftIO $! do
+        elm_win_util_standard_add (fromString "mainwnd") (fromString "hproxy") >>= \parent-> do
+            -- on close
+            evas_object_smart_callback_add parent (fromString "delete,request") (on_done outbox) nullPtr
+            -- set size
+            --evas_object_size_hint_min_set parent 640 480
+            evas_object_resize parent 300 80
+            -- box
+            box <- elm_box_add parent
+            evas_object_size_hint_weight_set box c'EVAS_HINT_EXPAND c'EVAS_HINT_EXPAND
+            -- resize box to parent
+            elm_win_resize_object_add parent box
+            evas_object_show box
+            
+            (loginBox, login) <- addLogin parent
+            elm_box_pack_end box loginBox
+            buttonsBox <- addButtons parent outbox
+            elm_box_pack_end box buttonsBox
+
+            evas_object_show parent
+        
+        elm_run
+        return ()
+    procFinished
 
 nullCallback:: EvasObjectSmartCallback
 nullCallback _ _ _ = return ()
 
-menuButtons:: [(String, EvasObjectSmartCallback)]
+menuButtons:: [(String, MBox SomeMessage-> EvasObjectSmartCallback)]
 menuButtons = 
-    [ ("Новый", newWindow)
-    , ("Редактировать", nullCallback)
-    , ("Удалить", nullCallback)
+    [ ("Ок", on_login)
     , ("Выход", on_done)
     ]
 
-addMenu:: Ptr C'Evas_Object-> IO (Ptr C'Evas_Object)
-addMenu parent = elm_box_add parent >>= \box-> do
+addButtons:: Ptr C'Evas_Object-> MBox SomeMessage-> IO (Ptr C'Evas_Object)
+addButtons parent outbox = elm_box_add parent >>= \box-> do
     elm_box_horizontal_set box
     evas_object_size_hint_weight_set box c'EVAS_HINT_EXPAND c'EVAS_HINT_EXPAND
     evas_object_size_hint_align_set box c'EVAS_HINT_FILL c'EVAS_HINT_FILL
     
+    
     forM_ menuButtons $ \(desc, func)-> do
         button <- elm_button_add parent
         elm_object_text_set button (fromString desc)
-        evas_object_smart_callback_add button (fromString "clicked") func nullPtr
+        evas_object_smart_callback_add button (fromString "clicked") (func outbox) nullPtr
         evas_object_size_hint_align_set button c'EVAS_HINT_FILL c'EVAS_HINT_FILL
         evas_object_size_hint_weight_set button c'EVAS_HINT_EXPAND 0.0
         elm_box_pack_end box button
@@ -66,14 +92,14 @@ addMenu parent = elm_box_add parent >>= \box-> do
     evas_object_show box
     return box
     
-addSearch:: Ptr C'Evas_Object-> IO (Ptr C'Evas_Object)
-addSearch parent = elm_box_add parent >>= \box-> do
+addLogin:: Ptr C'Evas_Object-> IO (Ptr C'Evas_Object, Ptr C'Evas_Object)
+addLogin parent = elm_box_add parent >>= \box-> do
     elm_box_horizontal_set box
-    evas_object_size_hint_weight_set box c'EVAS_HINT_EXPAND 0.0
+    evas_object_size_hint_weight_set box c'EVAS_HINT_EXPAND c'EVAS_HINT_EXPAND
     evas_object_size_hint_align_set box c'EVAS_HINT_FILL c'EVAS_HINT_FILL
     
     label <- elm_label_add parent
-    elm_object_text_set label $ fromString "Фильтр:"
+    elm_object_text_set label $ fromString "Логин в домене:"
     elm_box_pack_end box label
     evas_object_show label
 
@@ -87,27 +113,17 @@ addSearch parent = elm_box_add parent >>= \box-> do
     evas_object_show entry
     
     evas_object_show box
-    return box
+    return (box, entry)
     
-addStatus:: Ptr C'Evas_Object-> IO (Ptr C'Evas_Object)
-addStatus parent = do
-    label <- elm_label_add parent
-    evas_object_size_hint_weight_set label c'EVAS_HINT_EXPAND 0.0
-    evas_object_size_hint_align_set label c'EVAS_HINT_FILL c'EVAS_HINT_FILL
-    elm_object_text_set label $ fromString "here goes my status"
-    
-    evas_object_show label
-    return label
-
-newWindow:: EvasObjectSmartCallback
-newWindow _ _ _ = do
-    win <- elm_win_util_standard_add (fromString "newwnd") (fromString "новое подключение")
-    evas_object_smart_callback_add win (fromString "delete,request") on_closeNew nullPtr
-    evas_object_show win
 
 
 on_closeNew _ ptr _ = do
     evas_object_del ptr
 
-on_done _ _ _= do
+on_done mbox _ _ _ = do
+    sendMBox mbox $! toMessage GuiExit
     elm_exit
+
+on_login mbox _ ptr _ = do    
+    login <- elm_object_text_get ptr
+    sendMBox mbox $! toMessage $! GuiLogin login
